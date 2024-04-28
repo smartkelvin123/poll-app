52c2859801e95164724a7886bc3891d77ec30d57
unknown <smartkelvin66@gmail.com>
2024-04-28 05:42:01 +0100

    homescreen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,35 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import { Stack } from "expo-router";
 
 export default function HomeScreen() {
+  const polls = [1, 2, 3, 4, 5];
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Stack.Screen
+        options={{
+          title: "Home",
+          headerStyle: {
+            backgroundColor: "#fff",
+          },
+          headerTintColor: "#000",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      />
+      <View style={styles.container}>
+        <FlatList
+          data={polls}
+          contentContainerStyle={{ gap: 5 }}
+          renderItem={() => (
+            <View>
+              <Text>hello</Text>
+            </View>
+          )}
+        />
+      </View>
+    </>
   );
 }
 
